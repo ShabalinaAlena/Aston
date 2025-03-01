8b1b14a7555abde880273987f3f1c0ce8a2feb2c
*Shabalina <Al.o5o6@yandex.ru>
2025-03-01 21:55:23 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/org/example/EightClass.java
    #	src/main/java/org/example/ElevenClass.java
    #	src/main/java/org/example/FourClass.java
    #	src/main/java/org/example/FourteenClass.java
    #	src/main/java/org/example/NineClass.java
    #	src/main/java/org/example/SevenClass.java
    #	src/main/java/org/example/SixClass.java
    #	src/main/java/org/example/TenClass.java
    #	src/main/java/org/example/ThirteenClass.java
    #	src/main/java/org/example/ThreeClass.java
    #	src/main/java/org/example/TwelveClass.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,9 @@
-package org.example;
-
 public class Main {
+    public static void main(String[] args) {
+        Park park = new Park("Центральный парк");
+
+        park.addAttraction("Веревочный парк", "09:00 - 20:00", 250.0);
+        park.addAttraction("Вихрь", "10:00 - 20:00", 400.0);
+        park.addAttraction("Гусеница", "11:00 - 20:00", 200.0);
+    }
 }
