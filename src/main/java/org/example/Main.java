516829e5097de227cf99bfd18fbafe879b723fc3
*Shabalina <Al.o5o6@yandex.ru>
2025-03-05 22:00:58 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/org/example/EightClass.java
    #	src/main/java/org/example/ElevenClass.java
    #	src/main/java/org/example/FourClass.java
    #	src/main/java/org/example/FourteenClass.java
    #	src/main/java/org/example/NineClass.java
    #	src/main/java/org/example/SevenClass.java
    #	src/main/java/org/example/SixClass.java
    #	src/main/java/org/example/TenClass.java
    #	src/main/java/org/example/ThirteenClass.java
    #	src/main/java/org/example/ThreeClass.java
    #	src/main/java/org/example/TwelveClass.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+    //Задание 1
         Миска miska = new Миска(30);
 
         Собака tuzik = new Собака("Tuzik");
@@ -21,5 +21,28 @@ public class Main {
         murka.есть(miska);
 
         System.out.println("Всего животных: " + Животное.счетчик);
+
+    // Задание 2
+        Круг круг = new Круг(10, "белый", "черный");
+        Прямоугольник прямоугольник = new Прямоугольник(6, 8, "зеленый", "белый");
+        Треугольник треугольник = new Треугольник(4, 5, 6, "красный", "желтый");
+
+        System.out.println("\nКруг:");
+        System.out.println("Периметр: " + круг.периметр());
+        System.out.println("Площадь: " + круг.площадь());
+        System.out.println("Цвет заливки: " + круг.цветЗаливки);
+        System.out.println("Цвет границы: " + круг.цветГраницы);
+
+        System.out.println("\nПрямоугольник:");
+        System.out.println("Периметр: " + прямоугольник.периметр());
+        System.out.println("Площадь: " + прямоугольник.площадь());
+        System.out.println("Цвет заливки: " + прямоугольник.цветЗаливки);
+        System.out.println("Цвет границы: " + прямоугольник.цветГраницы);
+
+        System.out.println("\nТреугольник:");
+        System.out.println("Периметр: " + треугольник.периметр());
+        System.out.println("Площадь: " + треугольник.площадь());
+        System.out.println("Цвет заливки: " + треугольник.цветЗаливки);
+        System.out.println("Цвет границы: " + треугольник.цветГраницы);
     }
 }
